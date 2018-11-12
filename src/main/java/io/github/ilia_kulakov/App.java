772f9584aca0e41f3759efb02f07c5b526418f3c
Rinat Muhamedgaliev <rinat.muhamedgaliev@gmail.com>
2018-11-13 01:55:44 +0300

    Fixed reachability feature

--- src/main/java/io/github/ilia_kulakov/App.java
+++ src/main/java/io/github/ilia_kulakov/App.java
@@ -7,7 +7,6 @@ package io.github.ilia_kulakov;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 
--- src/main/java/io/github/ilia_kulakov/controller/UserControllerOne.java
+++ src/main/java/io/github/ilia_kulakov/controller/UserControllerOne.java
@@ -1,9 +1,9 @@
 package io.github.ilia_kulakov.controller;
 
-import io.github.ilia_kulakov.model.User;
 import io.github.ilia_kulakov.model.UserOne;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,7 +22,7 @@ public class UserControllerOne {
 
 
     @ResponseBody
-    @RequestMapping(value = "{userId}",method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "{userId}", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
     public UserOne updateUser(@PathVariable("userId") Integer userId) {
         UserOne user = this.users.get(userId);
         user.setContent("ranom-content");
@@ -32,7 +32,7 @@ public class UserControllerOne {
 
 
     @ResponseBody
-    @RequestMapping(value = "{userId}",method = RequestMethod.DELETE)
+    @RequestMapping(value = "{userId}", method = RequestMethod.DELETE)
     public void deleteUser(@PathVariable("userId") Integer userId) {
         this.users.remove(userId);
     }
