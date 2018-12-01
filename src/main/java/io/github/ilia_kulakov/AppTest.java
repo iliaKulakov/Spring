b4dbae9f10eb136aececf1398b88e1d04e187524
ilia <38052676+iliaKulakov@users.noreply.github.com>
2018-12-01 15:02:00 +0300

    Merge branch 'develop_task_1' of https://github.com/iliaKulakov/Spring into develop_task_1
    # Conflicts:
    #	src/main/java/io/github/ilia_kulakov/controller/UserControllerOne.java

--- src/main/java/io/github/ilia_kulakov/AppTest.java
+++ src/main/java/io/github/ilia_kulakov/AppTest.java
@@ -6,21 +6,37 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import static org.mockito.BDDMockito.*;
 
 //import static com.sun.org.apache.xerces.internal.util.PropertyState.is;
 //import static com.sun.org.apache.xerces.internal.util.PropertyState.is;
+import static com.sun.org.apache.xerces.internal.util.PropertyState.is;
 import static org.hamcrest.core.IsNull.notNullValue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.mockito.BDDMockito.given;
+
+import org.junit.*;
+import org.junit.runner.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.boot.test.context.*;
+import org.springframework.boot.test.mock.mockito.*;
+import org.springframework.test.context.junit4.*;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(GreetingController.class)
+@SpringBootTest
 public class AppTest {
 
     @Autowired
@@ -43,8 +59,10 @@ public class AppTest {
                 .andExpect(status().isOk())
                 .andDo(print())
                 .andExpect(jsonPath("content", notNullValue()))
-                .andExpect(jsonPath("content", is("Hello, name!")));
+                .andExpect((ResultMatcher) jsonPath("content", is("Hello, name!")));
     }
+
+
 
 
 }
