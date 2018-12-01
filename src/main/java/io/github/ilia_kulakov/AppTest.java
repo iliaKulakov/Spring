6f217a63d0e5a46855f2dbe4a4be6a47352dac64
ilia <38052676+iliaKulakov@users.noreply.github.com>
2018-12-01 14:35:14 +0300

    Merge branch 'develop_task_1' of https://github.com/iliaKulakov/Spring into develop_task_1
    # Conflicts:
    #	src/main/java/io/github/ilia_kulakov/controller/UserControllerOne.java

--- src/main/java/io/github/ilia_kulakov/AppTest.java
+++ src/main/java/io/github/ilia_kulakov/AppTest.java
@@ -1,4 +1,4 @@
-package tests;
+package io.github.ilia_kulakov;
 
 import io.github.ilia_kulakov.controller.GreetingController;
 import io.github.ilia_kulakov.model.Greeting;
@@ -11,20 +11,17 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
-import static org.mockito.BDDMockito.given;
+//import static com.sun.org.apache.xerces.internal.util.PropertyState.is;
+//import static com.sun.org.apache.xerces.internal.util.PropertyState.is;
+import static org.hamcrest.core.IsNull.notNullValue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
 @RunWith(SpringRunner.class)
 @WebMvcTest(GreetingController.class)
-public class AppTests {
-
+public class AppTest {
 
     @Autowired
     private MockMvc mvc;
@@ -36,6 +33,7 @@ public class AppTests {
 
     @Test
     public void testAppHasAGreeting() throws Exception {
+
         given(greetingController.greeting("name")).willReturn(new Greeting(0, "Hello, name!"));
 
 
@@ -47,4 +45,6 @@ public class AppTests {
                 .andExpect(jsonPath("content", notNullValue()))
                 .andExpect(jsonPath("content", is("Hello, name!")));
     }
+
+
 }
