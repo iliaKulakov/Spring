f787c54afb4b08e4ec9cdcfd58554b85eedd5919
Rinat Muhamedgaliev <rinat.muhamedgaliev@gmail.com>
2018-12-01 15:15:23 +0300

    Fix tests

--- src/test/java/io/github/ilia_kulakov/AppTest.java
+++ src/test/java/io/github/ilia_kulakov/AppTest.java
@@ -6,38 +6,24 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
-import static org.mockito.BDDMockito.*;
 
-//import static com.sun.org.apache.xerces.internal.util.PropertyState.is;
-//import static com.sun.org.apache.xerces.internal.util.PropertyState.is;
-import static com.sun.org.apache.xerces.internal.util.PropertyState.is;
-import static org.hamcrest.core.IsNull.notNullValue;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.mockito.BDDMockito.given;
 
-import org.junit.*;
-import org.junit.runner.*;
-import org.springframework.beans.factory.annotation.*;
-import org.springframework.boot.test.context.*;
-import org.springframework.boot.test.mock.mockito.*;
-import org.springframework.test.context.junit4.*;
-
-import static org.assertj.core.api.Assertions.*;
-import static org.mockito.BDDMockito.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(GreetingController.class)
-@SpringBootTest
 public class AppTest {
+
 
     @Autowired
     private MockMvc mvc;
@@ -49,7 +35,6 @@ public class AppTest {
 
     @Test
     public void testAppHasAGreeting() throws Exception {
-
         given(greetingController.greeting("name")).willReturn(new Greeting(0, "Hello, name!"));
 
 
@@ -59,10 +44,6 @@ public class AppTest {
                 .andExpect(status().isOk())
                 .andDo(print())
                 .andExpect(jsonPath("content", notNullValue()))
-                .andExpect((ResultMatcher) jsonPath("content", is("Hello, name!")));
+                .andExpect(jsonPath("content", is("Hello, name!")));
     }
-
-
-
-
 }
