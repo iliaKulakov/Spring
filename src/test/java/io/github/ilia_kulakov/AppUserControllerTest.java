f20ea920d5659e3ec4ec4d0a960cc7c910127ff9
ilia <38052676+iliaKulakov@users.noreply.github.com>
2018-12-02 09:02:22 +0300

    added test for UserOne app (draft)

--- src/test/java/io/github/ilia_kulakov/AppUserControllerTest.java
+++ src/test/java/io/github/ilia_kulakov/AppUserControllerTest.java
@@ -2,6 +2,8 @@ package io.github.ilia_kulakov;
 
 import io.github.ilia_kulakov.controller.GreetingController;
 import io.github.ilia_kulakov.controller.UserControllerOne;
+import io.github.ilia_kulakov.model.Greeting;
+import io.github.ilia_kulakov.model.UserOne;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.BDDMockito.given;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(UserControllerOne.class)
@@ -20,11 +24,14 @@ public class AppUserControllerTest {
     @MockBean
     private UserControllerOne userControllerOne;
 
-
     @Test
     public void testAppUserOne() throws Exception {
+       // given(userControllerOne.greeting("name")).willReturn(new Greeting(0, "Hello, name!"));
+        given(userControllerOne.createUser("test_user_1"))
+                .willReturn(new UserOne("test_user_1") );
 
     }
+
 
 
 }
