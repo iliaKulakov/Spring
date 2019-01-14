f8023299ce020beac5224dfa16b324e8b727c127
education20172017@gmail.com <Ltrf,hm_2018>
2019-01-15 00:12:22 +0300

    first 2019 commit update

--- src/main/java/io/github/ilia_kulakov/App.java
+++ src/main/java/io/github/ilia_kulakov/App.java
@@ -19,6 +19,8 @@ import org.springframework.context.annotation.ComponentScan;
 public class App {
 
 
+
+
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
