741d6390cd59a8f9cdef265d5f80467f1a9808eb
education20172017@gmail.com <Ltrf,hm_2018>
2019-01-16 23:17:55 +0300

    added hibernate 16.01.2019

--- src/main/java/io/github/ilia_kulakov/domain/Message.java
+++ src/main/java/io/github/ilia_kulakov/domain/Message.java
@@ -1,16 +1,15 @@
 package io.github.ilia_kulakov.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity // This tells Hibernate to make a table out of this class
+@Table(name = 'my_first_table')
 public class Message {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer id;
 
+    @Column(name = "content")
     private String Content;
 
 
