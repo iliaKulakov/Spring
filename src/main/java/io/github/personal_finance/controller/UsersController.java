8e5c367270d48454f57e618b380f85193dcb20f5
Rinat Muhamedgaliev <rinat.muhamedgaliev@gmail.com>
2019-01-25 00:41:44 +0300

    Removed unused code

--- src/main/java/io/github/personal_finance/controller/UsersController.java
+++ src/main/java/io/github/personal_finance/controller/UsersController.java
@@ -5,7 +5,6 @@ import io.github.personal_finance.controller.dto.UserUpdateDTO;
 import io.github.personal_finance.domain.User;
 import io.github.personal_finance.repos.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
