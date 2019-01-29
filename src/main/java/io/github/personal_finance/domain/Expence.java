072daea1fe2e8dc644afb0eb152848739fb3bf1a
education20172017@gmail.com <Ltrf,hm_2018>
2019-01-29 23:44:31 +0300

    Added cascadeType for Expence class

--- src/main/java/io/github/personal_finance/domain/Expence.java
+++ src/main/java/io/github/personal_finance/domain/Expence.java
@@ -15,45 +15,9 @@ public class Expence {
     @JoinColumn(name = "category_id")
     private Category category;
 
-
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "expence")
     @Column(name = "user_id")
     private List<User> users;
-
-   // public Expence(Category category_id) {
-   //     this.category = category_id;
-   // }
-
-
-/*
-    public Expence() {
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    public void setCategory(Category category) {
-        this.category = category;
-    }
-
-
-
-    public List<User> getUsers() {
-        return users;
-    }
-
-    public void setUsers(List<User> users) {
-        this.users = users;
-    }*/
 
     public Expence(Category category) {
         this.category = category;
