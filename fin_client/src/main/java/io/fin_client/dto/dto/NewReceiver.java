554f40b2f22d2c4dc6615cc4882acb4feee01ede
Rinat Muhamedgaliev <rinat.muhamedgaliev@gmail.com>
2019-02-17 22:01:20 +0300

    Fixed recieve multiple queues

--- fin_client/src/main/java/io/fin_client/dto/dto/NewReceiver.java
+++ fin_client/src/main/java/io/fin_client/dto/dto/NewReceiver.java
@@ -1,41 +1,41 @@
-package io.fin_client.dto.dto;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.rabbitmq.client.*;
-import io.github.personal_finance.controller.dto.ExpenceCreateInfo;
-
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-
-public class NewReceiver {
-
-    private static final String TASK_QUEUE_NAME = "personal-finance";
-    static final String topicExchangeName = "personal-finance";
-
-    public static void main(String[] argv) throws Exception {
-
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("localhost");
-        Connection connection = factory.newConnection();
-        Channel channel = connection.createChannel();
-        channel.queueBind(TASK_QUEUE_NAME, topicExchangeName, "");
-        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
-
-        //Как этот кусок работает
-        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-            String message = new String(delivery.getBody(), "UTF-8");
-            System.out.println(" [x] Received '" + message + "'");
-        };
-
-        //channel.basicConsume(TASK_QUEUE_NAME, true, deliverCallback, consumerTag -> { });
-        //почему
-
-        channel.basicConsume(TASK_QUEUE_NAME, true, deliverCallback, new CancelCallback() {
-            @Override
-            public void handle(String consumerTag) throws IOException {
-            }
-        });
-
-    }
-
-        }
+//package io.fin_client.dto.dto;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.rabbitmq.client.*;
+//import io.github.personal_finance.controller.dto.ExpenceCreateInfo;
+//
+//import java.io.IOException;
+//import java.nio.charset.StandardCharsets;
+//
+//public class NewReceiver {
+//
+//    private static final String TASK_QUEUE_NAME = "personal-finance";
+//    static final String topicExchangeName = "personal-finance";
+//
+//    public static void main(String[] argv) throws Exception {
+//
+//        ConnectionFactory factory = new ConnectionFactory();
+//        factory.setHost("localhost");
+//        Connection connection = factory.newConnection();
+//        Channel channel = connection.createChannel();
+//        channel.queueBind(TASK_QUEUE_NAME, topicExchangeName, "");
+//        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+//
+//        //Как этот кусок работает
+//        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+//            String message = new String(delivery.getBody(), "UTF-8");
+//            System.out.println(" [x] Received '" + message + "'");
+//        };
+//
+//        //channel.basicConsume(TASK_QUEUE_NAME, true, deliverCallback, consumerTag -> { });
+//        //почему
+//
+//        channel.basicConsume(TASK_QUEUE_NAME, true, deliverCallback, new CancelCallback() {
+//            @Override
+//            public void handle(String consumerTag) throws IOException {
+//            }
+//        });
+//
+//    }
+//
+//        }
--- src/main/java/io/github/personal_finance/App.java
+++ src/main/java/io/github/personal_finance/App.java
@@ -13,6 +13,7 @@ import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -23,45 +24,32 @@ import org.springframework.context.annotation.Profile;
 @SpringBootApplication(scanBasePackages = "io.github.personal_finance")
 public class App {
 
-    static final String topicExchangeName = "personal-finance";
     static final String queueName = "personal-finance";
-    static final String topicExchangeNameArrival = "personal-finance-arrival";
     static final String queueNameArrival = "personal-finance-arrival";
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
 
-//    @Bean
-//    Queue queue() {
-//        return new Queue(queueName, false);
-//    }
-//
-//    @Bean
-//    TopicExchange exchange() {
-//        return new TopicExchange(topicExchangeName);
-//    }
-//
-//    @Bean
-//    Binding binding(Queue queue, TopicExchange exchange) {
-//        return BindingBuilder.bind(queue).to(exchange).with("foo.bar.#");
-//    }
-//
-//    @Bean
-//    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
-//                                             MessageListenerAdapter listenerAdapter) {
-//        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-//        container.setConnectionFactory(connectionFactory);
-//        container.setQueueNames(queueName);
-//        container.setMessageListener(listenerAdapter);
-//        return container;
-//    }
-//
-//    @Bean
-//    MessageListenerAdapter listenerAdapter(ExpenseReceiver receiver) {
-//        return new MessageListenerAdapter(receiver, "receiveMessage");
-//    }
-///
+    @Bean
+    Queue queue() {
+        return new Queue(queueName, false);
+    }
+
+    @Bean
+    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+                                            @Qualifier("listenerAdapter") MessageListenerAdapter listenerAdapter) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.setQueueNames(queueName);
+        container.setMessageListener(listenerAdapter);
+        return container;
+    }
+
+    @Bean
+    MessageListenerAdapter listenerAdapter(ExpenseReceiver receiver) {
+        return new MessageListenerAdapter(receiver, "receiveMessage");
+    }
 
 
     @Bean
@@ -70,19 +58,8 @@ public class App {
     }
 
     @Bean
-    TopicExchange exchangeArrival() {
-        return new TopicExchange(topicExchangeNameArrival);
-    }
-
-
-    @Bean
-    Binding bindingArrival(Queue queueArrival, TopicExchange exchangeArrival) {
-        return BindingBuilder.bind(queueArrival).to(exchangeArrival).with("foo.bar.#");
-    }
-
-    @Bean
     SimpleMessageListenerContainer containerArrival(ConnectionFactory connectionFactory,
-                                             MessageListenerAdapter listenerAdapter) {
+                                            @Qualifier("listenerAdapterArrival") MessageListenerAdapter listenerAdapter) {
         SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
         container.setConnectionFactory(connectionFactory);
         container.setQueueNames(queueNameArrival);
