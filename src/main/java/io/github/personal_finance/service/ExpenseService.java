07c108fcea80122b78ea8a3ff7e489b22f736518
education20172017@gmail.com <Ltrf,hm_2018>
2019-02-23 19:10:29 +0300

    Added method in interface - createArrival

--- src/main/java/io/github/personal_finance/service/ExpenseService.java
+++ src/main/java/io/github/personal_finance/service/ExpenseService.java
@@ -1,9 +1,14 @@
 package io.github.personal_finance.service;
 
+import io.github.personal_finance.controller.dto.ArrivalCreateInfo;
 import io.github.personal_finance.controller.dto.ExpenceCreateInfo;
+import io.github.personal_finance.domain.Arrival;
 import io.github.personal_finance.domain.Expense;
+import io.github.personal_finance.domain.User;
 
 public interface ExpenseService {
 
     Expense createExpense(ExpenceCreateInfo expenceCreateInfo);
+
+    Arrival createArrival(ArrivalCreateInfo arrivalCreateInfo);
 }
